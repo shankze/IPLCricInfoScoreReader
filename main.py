e09c8e79f13f1419525951d9daaec9ec7d564a1c
Shashank <shankze@gmail.com>
2023-04-03 01:33:39 -0500

    Added File based Batsman List

--- main.py
+++ main.py
@@ -7,32 +7,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-LSG_player_list = [
-    "KL Rahul",
-    "Kyle Mayers",
-    "Deepak Hooda",
-    "Krunal Pandya",
-    "Marcus Stoinis",
-    "Nicholas Pooran",
-    "Ayush Badoni",
-    "Krishnappa Gowtham"
-]
+page_url = "https://www.espncricinfo.com/series/indian-premier-league-2023-1345038/royal-challengers-bangalore-vs-mumbai-indians-5th-match-1359479/full-scorecard"
 
-def get_page_content():
-    page = requests.get("https://www.espncricinfo.com/series/indian-premier-league-2023-1345038/lucknow-super-giants-vs-delhi-capitals-3rd-match-1359477/full-scorecard")
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="main-container")
-    #print(results)
-    #score = results.find_all("div", class_="ci-scorecard-table")
-    #print(score)
-    #for table in results.find_all('table'):
-    #    print(table.get('class'))
+report_file_name_list = {
+    "Mumbai Indians": "MI_batsman_list.txt",
+    "Royal Challengers Bangalore": "RCB_batsman_list.txt",
+    "Chennai Super Kings":"CSK_batsman_list.txt",
+    "Gujarat Titans":"GT_batsman_list.txt",
+    "Punjab Kings":"PBKS_batsman_list.txt",
+    "Kolkata Knight Riders":"KKR_batsman_list.txt",
+    "Lucknow Super Giants":"LSG_batsman_list.txt",
+    "Delhi Capitals":"DC_batsman_list.txt",
+    "Rajasthan Royals":"RR_batsman_list.txt",
+    "Sunrisers Hyderabad": "SRH_batsman_list.txt"
+}
+
+
+def get_team_names(soup):
+    spans = soup.find_all('span', {'class': 'ds-text-title-xs ds-font-bold ds-capitalize'})
+    team_names = []
+    for span in spans:
+        team_names.append(span.text)
+    return team_names
+
+def get_batting_scores(soup):
     tables = soup.find_all('table')
     for table in tables:
         print(table.get('class'))
     score_tables = soup.find_all('table', class_='ci-scorecard-table')
     print(score_tables)
 
+    scores_list = []
     for score_table in score_tables:
         rows = score_table.findChildren(['tr'])
         scores = {}
@@ -41,33 +46,100 @@ def get_page_content():
             if len(columns) > 4:
                 col_0 = columns[0]
                 name_link = col_0.findChildren(['a'])
-                not_out  = False
-                player_score = 0
                 if (columns[1].text.strip() == 'not out'):
-                    #print(columns[2].text.strip(),'*')
-                    player_score = columns[2].text.strip()+'*'
+                    player_score = columns[2].text.strip() + '*'
                 else:
-                    #print(columns[2].text.strip())
                     player_score = columns[2].text.strip()
-                scores[name_link[0].get('title')] =player_score
-        print(scores)
+                scores[name_link[0].get('title')] = player_score
+        scores_list.append(scores)
+    return scores_list
 
-
-        '''
-        for row in score_table.tbody.find_all('tr'):
-            # Find all data for each column
-            columns = row.find_all('td')
-            print(columns[0])
-        '''
+def get_page_content():
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    team_names = get_team_names(soup)
+    batting_scores = get_batting_scores(soup)
+    return team_names,batting_scores
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def get_player_list_from_file(team_name):
+    report_batsmen_list_file_name = report_file_name_list[team_name]
+    f = open(report_batsmen_list_file_name, "r")
+    text = f.read()
+    f.close()
+    player_list = []
+    if "," in text:
+        player_list = text.split(",")
+    return player_list
 
-# Press the green button in the gutter to run the script.
+
+def generate_batting_report(team_names,batting_scores):
+    batting_reports = []
+    for i in range(0, 2):
+        report_batsmen_list = get_player_list_from_file(team_names[i])
+        #report_batsmen_list = player_list[team_names[i]]
+        #print(report_batsmen_list)
+        batting_report = {}
+        #generate for players already in report list
+        for batsman in report_batsmen_list:
+            if batsman in batting_scores[i]:
+                batting_report[batsman] = batting_scores[i][batsman]
+                #print(batting_scores[i][batsman])
+            else:
+                batting_report[batsman] = " "
+                #print(" ")
+        #for new players playing first time, add them to the end of the list
+        for batsman in batting_scores[i]:
+            if batsman not in batting_report:
+                batting_report[batsman] = batting_scores[i][batsman]
+        batting_reports.append(batting_report)
+    return batting_reports
+
+def print_batting_reports(batting_reports):
+    for report in batting_reports:
+        '''
+        for batsman in report:
+            print(batsman)
+        print("-----")
+        '''
+        for batsman in report:
+            print(report[batsman])
+
+        formatted_batsman_list = ""
+        contains_at_least_one_element = False
+        for batsman in report:
+            if contains_at_least_one_element == True:
+                formatted_batsman_list = formatted_batsman_list + "," + batsman
+            else:
+                formatted_batsman_list = formatted_batsman_list + batsman
+                contains_at_least_one_element = True
+        formatted_batsman_list  = formatted_batsman_list
+        print(formatted_batsman_list)
+        print("=======")
+
+def write_player_list_to_file(team_names,batting_reports):
+    for i in range(0, 2):
+        file_name = report_file_name_list[team_names[i]]
+        formatted_batsman_list = ""
+        contains_at_least_one_element = False
+        for batsman in batting_reports[i]:
+            if contains_at_least_one_element == True:
+                formatted_batsman_list = formatted_batsman_list + "," + batsman
+            else:
+                formatted_batsman_list = batsman
+                contains_at_least_one_element = True
+        f = open(file_name, "w")
+        f.write(formatted_batsman_list)
+        f.close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    get_page_content()
+    team_names, batting_scores = get_page_content()
+    batting_reports = generate_batting_report(team_names,batting_scores)
+    print_batting_reports(batting_reports)
+    write_player_list_to_file(team_names, batting_reports)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
